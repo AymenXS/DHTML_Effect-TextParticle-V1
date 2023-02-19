2dde101a09363433694ed2f8f3b02fb241b053c0
AymenXS <aymenghaloua@gmail.com>
2023-02-19 10:48:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const text = "Hello!";
+});
